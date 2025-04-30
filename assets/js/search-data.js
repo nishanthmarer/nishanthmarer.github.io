b92e066837f411b17addcd1f86f69c2157e796b3
nishanthmarer <125854808+nishanthmarer@users.noreply.github.com>
2025-04-30 17:02:38 +0000

    Deploying to gh-pages from @ nishanthmarer/nishanthmarer.github.io@e23dc1b2375c70d5bcc6984cd38752c41efa85f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,4 +419,30 @@ ninja.data = [{
           description: "Shallow Convolutional Neural Network for Image Classification",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Shallow_Nets/";
-            },},];
+            },},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
